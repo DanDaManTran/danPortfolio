4acb20e793f9e3f2279503e01f361c471571e30f
DanDaManTran <DannDoesntKnow@dans-mbp.home>
2016-12-19 14:36:41 -0600

    adding files

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+$("document").ready(function(){
+
+	//slowly scrolls to other pages when clicked on the top header
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+			if (target.length) {
+				$('html, body').animate({scrollTop: target.offset().top}, 1000);
+				return false;
+			}
+		}
+	});
+
+	//Hide and show text on the edEx page
+	$(document).on("click","button",function(){
+	  	var num = $(this).data('value');
+
+	  	if(num%2==0){
+	  		$(this).text("-");
+	  		$(this).data('value',(num+1));
+	  		$('[data-showHide='+num+']').show();
+	  	} else{
+	  		$(this).text("+");
+	  		$(this).data('value',(num-1));
+	  		$('[data-showHide='+(num-1)+']').hide();
+	  	}
+	});
+
+	//show title, github and heroku links for each project, when mouse is over the project picture
+	$(".backPort").on("mouseover", function(){
+		var portIndex = $(this).data('value');
+		$("#"+portIndex).show();
+	});
+
+	//hide title, github and heroku links for each project, when mouse move out the project picture
+	$(".backPort").on("mouseout", function(){
+		var portIndex = $(this).data('value');
+		$("#"+portIndex).hide();
+	});
+
+
+
+
+});
